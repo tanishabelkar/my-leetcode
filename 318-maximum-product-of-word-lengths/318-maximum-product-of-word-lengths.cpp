55748d778ab76a036e3dcdd4429fd60be927edc6
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-05-29 15:16:39 +0530

    Time: 64 ms (64.78%), Space: 15.7 MB (79.69%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,30 @@
+class Solution 
+{
+public:
+    int maxProduct(vector<string>& words) 
+    {
+        vector<int> rep(words.size(),0);
+        for(int i=0;i<words.size();++i)
+        {
+            for(int j=0;j<words[i].size();++j)
+            {
+                int bit=words[i][j]-'a';
+                rep[i]|=(1<<bit);
+            }
+        }
+        //for(int i=0;i<rep.size();++i) cout<<rep[i]<<" ";
+        int res=0;
+        for(int i=0;i<rep.size();i++)
+        {
+            for(int j=i+1;j<rep.size();j++)
+            {
+                if((rep[i]&rep[j])==0)
+                {
+                    if(res<words[i].size()*words[j].size())
+                        res=words[i].size()*words[j].size();
+                }
+            }
+        }
+        return res;
+    }
+};
