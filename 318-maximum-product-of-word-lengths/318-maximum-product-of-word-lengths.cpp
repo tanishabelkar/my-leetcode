6768d072604c8081e5740f645c5f1403dd97b496
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-05-29 15:26:57 +0530

    Time: 96 ms (42.93%), Space: 15.6 MB (89.46%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,31 +1,30 @@
 class Solution
 {
     public:
-        int stringToInt(string s)
+        int maxProduct(vector<string> &words)
         {
-            int res = 0;
-            for (int j = 0; j < s.size(); ++j)
+            vector<int> rep(words.size(), 0);
+            for (int i = 0; i < words.size(); ++i)
             {
-                int bit = s[j] - 'a';
-                res |= (1 << bit);
+                for (int j = 0; j < words[i].size(); ++j)
+                {
+                    int bit = words[i][j] - 'a';
+                    rep[i] |= (1 << bit);
+                }
+            }
+           	//for(int i=0;i < rep.size();++i) cout<<rep[i]<<" ";
+            int res = 0;
+            for (int i = 0; i < rep.size(); i++)
+            {
+                for (int j = i + 1; j < rep.size(); j++)
+                {
+                    if ((rep[i] &rep[j]) == 0)
+                    {
+                        if (res < words[i].size() *words[j].size())
+                            res = words[i].size() *words[j].size();
+                    }
+                }
             }
             return res;
         }
-
-    int maxProduct(vector<string> &words)
-    {
-        int res = 0;
-        for (int i = 0; i < words.size(); i++)
-        {
-            for (int j = i + 1; j < words.size(); j++)
-            {
-                if ((stringToInt(words[i]) & stringToInt(words[j])) == 0)
-                {
-                    if (res < words[i].size() *words[j].size())
-                        res = words[i].size() *words[j].size();
-                }
-            }
-        }
-        return res;
-    }
 };
