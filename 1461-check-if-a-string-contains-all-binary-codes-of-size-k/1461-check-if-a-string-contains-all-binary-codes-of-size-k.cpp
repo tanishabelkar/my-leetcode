aafa1ec65fbaa5fbf96878f26b6585ad530c23f0
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-05-31 11:30:08 +0530

    Time: 1009 ms (21.38%), Space: 64.2 MB (55.66%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+public:
+    
+    int toInt(string s)
+    {
+        //cout<<s<<" ";
+        int res=0;
+        for(int i=0;i<s.size();++i)
+        {
+            if(s[i]=='1')
+                res|=(1<<(s.size()-i-1));
+        }
+        return res;
+    }
+    
+    bool hasAllCodes(string s, int k) 
+    {
+        if(k>s.size())
+            return false;
+        set<int> st;
+        for(int i=0;i<=s.size()-k;++i)
+            st.insert(toInt(s.substr(i,k)));
+        //cout<<st.size()<<" ";
+        return st.size()==(1<<k);
+    }
+};
