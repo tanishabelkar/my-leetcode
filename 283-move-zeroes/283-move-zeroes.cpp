666e3042c6caa3071a23b74106684a84b1ae89d9
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-06-05 19:08:07 +0530

    Time: 44 ms (25.56%), Space: 19.1 MB (98.02%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,19 +1,19 @@
-class Solution 
+class Solution
 {
-public:
-    void moveZeroes(vector<int>& nums) 
-    {
-        int n=nums.size(), i=-1, j=0;
-        while(j<n)
+    public:
+        void moveZeroes(vector<int> &nums)
         {
-            if(nums[j]!=0)
+            int n = nums.size(), i = -1, j = 0;
+            while (j < n)
             {
-                ++i;
-                int temp=nums[j];
-                nums[j]=nums[i];
-                nums[i]=temp;
+                if (nums[j] != 0)
+                {
+                    ++i;
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+                ++j;
             }
-            ++j;
         }
-    }
 };
