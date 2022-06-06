3f9ae945bc2831b5ef9eaf44244d7b228eda87ee
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-06-07 00:04:23 +0530

    Time: 2576 ms (5.03%), Space: 649.4 MB (5.02%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -0,0 +1,41 @@
+class Solution 
+{
+public:
+    
+    vector<vector<int>> visit;
+    
+    int getArea(vector<vector<int>> grid, int r, int c)
+    {
+        if(r>=grid.size() || r<0)
+            return 0;
+        if(c>=grid[0].size() || c<0)
+            return 0;
+        if(visit[r][c]==1)
+            return 0;
+        
+        visit[r][c]=1;
+        if(grid[r][c]==0)
+            return 0;
+        else return 1+getArea(grid, r+1, c)+getArea(grid, r-1, c)+getArea(grid, r, c+1)+getArea(grid, r, c-1);
+        
+    }  
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) 
+    {
+        visit = grid;
+        for(int i=0;i<visit.size();++i)
+            for(int j=0;j<visit[i].size();++j) visit[i][j]=0;
+        
+        int ans=0;
+        for(int i=0;i<grid.size();++i)
+        {
+            for(int j=0;j<grid[i].size();++j)
+            {
+                if(grid[i][j]==1 && visit[i][j]==0)
+                    ans=max(ans, getArea(grid,i,j));
+            }
+        }
+        
+        return ans;
+    }
+};
