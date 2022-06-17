551a21a68e8940854ae1f5673abd75d4a02a1ca0
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-06-17 23:59:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.06%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) 
+    {
+        if(n<=1)
+            return n;
+        int fib[n+1];
+        fib[0]=0;
+        fib[1]=1;
+        for(int i=2;i<=n;++i)
+            fib[i]=fib[i-2]+fib[i-1];
+        return fib[n];
+    }
+};
