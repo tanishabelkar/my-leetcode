92ccdfde1f708822218c1f47b397c610b24f20d3
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-07-15 13:45:20 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (92.66%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,22 +2,13 @@ class Solution
 {
 public:
     
-    void swap(int &a, int &b)
-    {
-        int temp=a;
-        a=b;
-        b=temp;
-    }
-    
     void rotate(vector<vector<int>>& matrix) 
     {
         //transpose
         for(int i=0;i<matrix.size();++i)
         {
             for(int j=i;j<matrix[i].size();++j)
-            {
                 swap(matrix[i][j], matrix[j][i]);
-            }
         }
         
         //flip
