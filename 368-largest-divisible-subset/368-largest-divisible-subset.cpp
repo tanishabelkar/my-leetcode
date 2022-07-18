d32d1b553ceb8230e2a24e34cceb49bb459fdc23
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-07-18 19:08:26 +0530

    Time: 100 ms (24.15%), Space: 10.8 MB (10.43%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -4,8 +4,10 @@ class Solution
         vector<int> largestDivisibleSubset(vector<int> &nums)
         {
             sort(nums.begin(), nums.end());
-            vector<vector < int>> dp(nums.size());
-            dp[0].push_back(nums[0]);
+            
+            vector<vector < int> > dp(nums.size());
+            dp[0].push_back(nums[0]); //base case
+            
             for (int i = 1; i < nums.size(); ++i)
             {
                 for (int j = i - 1; j >= 0; --j)
@@ -19,13 +21,13 @@ class Solution
                         }
                     }
                 }
-                if (dp[i].size() == 0) dp[i].push_back(nums[i]);
-                /*for(auto d:dp[i]) cout<<d<<" ";
-                cout<<"\n"; */
+                
+                if (dp[i].size() == 0) //incase no element divides current element
+                    dp[i].push_back(nums[i]);
             }
 
             int ml = dp[0].size(), mi = 0;
-            for (int i = 1; i < dp.size(); ++i)
+            for (int i = 1; i < dp.size(); ++i) //find maximum sized vector
             {
                 if (ml < dp[i].size())
                 {
