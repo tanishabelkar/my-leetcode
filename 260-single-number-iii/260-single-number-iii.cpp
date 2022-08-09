cba635d2f123224eb09243de8d4df3527ccd760b
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-08-10 00:38:35 +0530

    Time: 18 ms (34.97%), Space: 10.1 MB (43.63%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,32 +1,33 @@
-class Solution {
-public:
-    bool isSet(int n, int k)
-    {
-        return n&(1<<k);
-    }
-    
-    vector<int> singleNumber(vector<int>& nums) 
-    {
-        if(nums.size() == 2)
-            return nums;
-        
-        int x=nums[0];
-        vector<int> res(2,0);
-        for(int i=1;i<nums.size();++i)
-            x=(x^nums[i]);
-        
-        int pos=0;
-        while(pos<31)
+class Solution
+{
+    public:
+        bool isSet(int n, int k)
         {
-            if(isSet(x, pos))
+            return n & (1 << k);
+        }
+
+    vector<int> singleNumber(vector<int> &nums)
+    {
+        if (nums.size() == 2)
+            return nums;
+
+        int x = nums[0];
+        vector<int> res(2, 0);
+        for (int i = 1; i < nums.size(); ++i)
+            x = (x ^ nums[i]);
+
+        int pos = 0;
+        while (pos < 31)
+        {
+            if (isSet(x, pos))
                 break;
             ++pos;
         }
-        for(int i=0;i<nums.size();++i)
+        for (int i = 0; i < nums.size(); ++i)
         {
-            if(isSet(nums[i], pos))
-                res[0]=(res[0]^nums[i]);
-            else res[1]=(res[1]^nums[i]);
+            if (isSet(nums[i], pos))
+                res[0] = (res[0] ^ nums[i]);
+            else res[1] = (res[1] ^ nums[i]);
         }
         return res;
     }
