0d27e565c4945dd818c52c01d4e235359ac6df30
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-09-09 20:10:03 +0530

    Time: 58 ms (16.74%), Space: 22.3 MB (91.52%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,11 +1,22 @@
-class Solution 
+class Solution
 {
-public:
-    int singleNonDuplicate(vector<int>& nums) 
-    {
-        int x = nums[0];
-        for(int i=1;i<nums.size();++i)
-            x = (x^nums[i]);
-        return x;
-    }
+    public:
+        int singleNonDuplicate(vector<int> &nums)
+        {
+           	// all elements appear atmost twice!!
+
+            int beg = 0, end = nums.size() - 1, mid;
+            while (beg <= end)
+            {
+                mid = beg + (end - beg) / 2;
+                if (mid == 0 || mid == nums.size() - 1 || (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]))
+                    return nums[mid];
+
+                else if ((mid % 2 == 0 && nums[mid] == nums[mid - 1]) || (mid % 2 == 1 && nums[mid] == nums[mid + 1]))
+                    end = mid - 1;
+                else beg = mid + 1;
+            }
+
+            return -1;
+        }
 };
