7eb34ab40f78682b9d002fa4739ac7ed9de60d9b
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-09-22 11:57:06 +0530

    Time: 101 ms (25.11%), Space: 25.7 MB (26.53%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,27 +1,27 @@
-class Solution 
+class Solution
 {
-    vector<vector<int>> res;
-public:
-    
-    void backtrack(vector<int> can, vector<int> temp, int idx, int currSum, int k)
-    {
-        if(currSum > k)
-            return;
-        else if (currSum == k)
+    vector<vector < int>> res;
+    public:
+
+        void backtrack(vector<int> can, vector<int> temp, int ind, int currSum, int k)
         {
-            res.push_back(temp);
-            return;
+            if (currSum > k)
+                return;
+            else if (currSum == k)
+            {
+                res.push_back(temp);
+                return;
+            }
+
+            for (int i = ind; i < can.size(); ++i)
+            {
+                temp.push_back(can[i]);
+                backtrack(can, temp, i, currSum + can[i], k);
+                temp.pop_back();
+            }
         }
-        
-        for (int i = idx; i < can.size(); ++i)
-        {
-            temp.push_back(can[i]);
-            backtrack(can, temp, i, currSum + can[i], k);
-            temp.pop_back();
-        }
-    }
-    
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+
+    vector<vector < int>> combinationSum(vector<int> &candidates, int target)
     {
         backtrack(candidates, {}, 0, 0, target);
         return res;
