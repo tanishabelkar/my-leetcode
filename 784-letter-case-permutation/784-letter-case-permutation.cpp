49f048c729022d5db65f4f0e865846bae15c4f10
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-09-26 10:54:05 +0530

    Time: 13 ms (44.86%), Space: 9.9 MB (81.87%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+    vector<string> res;
+    char changeCase(char c)
+    {
+        if (c >= 'a' && c <= 'z')
+            return toupper(c);
+        else return tolower(c);
+    }
+    void backtrack(string temp, int idx)
+    {
+        res.push_back(temp);
+
+        if (idx >= temp.size())
+            return;
+
+        for (int i = idx; i < temp.size(); ++i)
+        {
+            if (isalpha(temp[i]))
+            {
+                temp[i] = changeCase(temp[i]);
+                backtrack(temp, i + 1);
+                temp[i] = changeCase(temp[i]);
+            }
+        }
+    }
+    public:
+        vector<string> letterCasePermutation(string s)
+        {
+            backtrack(s, 0);
+            return res;
+        }
+};
