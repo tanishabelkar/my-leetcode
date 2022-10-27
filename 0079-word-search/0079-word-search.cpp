9083d5d7bcaa557d106951e99684de68f93c2bd6
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-10-27 17:47:23 +0530

    Time: 1368 ms (24.13%), Space: 8 MB (48.38%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,35 +3,32 @@ class Solution
     bool backtrack(vector<vector < char>> &board, string w, int ind, int i, int j)
     {
         if (ind >= w.size())
-        {
             return true;
-        }
         if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size())
             return false;
         if (w[ind] != board[i][j])
             return false;
+
         board[i][j] = '*';
-        bool ans = backtrack(board, w, ind + 1, i + 1, j) || 
-            backtrack(board, w, ind + 1, i - 1, j) || 
-            backtrack(board, w, ind + 1, i, j + 1) || 
+        bool ans = backtrack(board, w, ind + 1, i + 1, j) ||
+            backtrack(board, w, ind + 1, i - 1, j) ||
+            backtrack(board, w, ind + 1, i, j + 1) ||
             backtrack(board, w, ind + 1, i, j - 1);
         board[i][j] = w[ind];
+
         return ans;
     }
     public:
         bool exist(vector<vector < char>> &board, string word)
         {
-            bool res;
             for (int i = 0; i < board.size(); ++i)
             {
                 for (int j = 0; j < board[0].size(); ++j)
                 {
-                    res = backtrack(board, word, 0, i, j);
-                    if (res)
+                    if (backtrack(board, word, 0, i, j))
                         return true;
                 }
             }
-
             return false;
         }
 };
