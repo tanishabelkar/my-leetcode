9f0f44ab4c413d316c3144dec42b1eb4bafdb2d8
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-11-07 16:11:02 +0530

    Time: 115 ms (64.64%), Space: 44.4 MB (12.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,10 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  for (var i = 0; i < nums.length; i++) {
-    const ind = nums.findIndex(function (e) {
-      return e === target - nums[i];
-    });
-    if (ind !== -1 && ind !== i) return Array(i, ind);
-  }
+  let dic = new Map();
+  nums.forEach((n, i) => {
+    dic.set(n, i);
+  });
+
+  let ans = [];
+
+  nums.forEach((n, i) => {
+    if (dic.has(target - n) && dic.get(target - n) !== i) {
+      ans = Array(i, dic.get(target - n));
+    }
+  });
+  return ans;
 };
