aa9d63fdbdb3922335aac30becd9550e9720c0fc
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-11-08 16:04:44 +0530

    Time: 5 ms (38.95%), Space: 6.4 MB (71.80%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,36 @@
+class Solution 
+{
+public:
+    string smallestSubsequence(string s) 
+    {
+        stack<char> st;
+        map<char, int> lastInd; //store the last index of each char
+        for (int i = 0; i < s.size(); ++i) lastInd[s[i]] = i;
+        
+        map<char, bool> vis;
+        
+        for (int i = 0; i < s.size(); ++i)
+        {
+            if (vis[s[i]])
+                continue;
+            
+            while(!st.empty() && st.top() > s[i] && lastInd[st.top()] > i)
+            {
+                vis[st.top()] = false;
+                st.pop();
+            }
+            
+            st.push(s[i]);
+            vis[s[i]] = true;
+        }
+        
+        string res = "";
+        while (!st.empty())
+        {
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
