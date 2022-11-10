ddf14232f358087581bf09905c4fe1338a1801de
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-11-10 19:56:36 +0530

    Time: 1176 ms (5.96%), Space: 780.9 MB (5.20%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,28 +1,23 @@
-class Solution 
+class Solution
 {
-public:
-    string removeDuplicates(string s) 
-    {
-        stack<char> st;
-        for (int i = 0; i < s.size(); ++i)
+    public:
+        string removeDuplicates(string s)
         {
-            if(st.empty())
-                st.push(s[i]);
-            else
+            stack<char> st;
+            for (int i = 0; i < s.size(); ++i)
             {
-                if (st.top() == s[i])
+                if (!st.empty() && st.top() == s[i])
                     st.pop();
-                else st.push(s[i]);
+                else
+                    st.push(s[i]);
             }
+
+            string res = "";
+            while (!st.empty())
+            {
+                res = st.top() + res;
+                st.pop();
+            }
+            return res;
         }
-        
-        string res="";
-        while(!st.empty())
-        {
-            res.push_back(st.top());
-            st.pop();
-        }
-        reverse(res.begin(), res.end());
-        return res;
-    }
 };
