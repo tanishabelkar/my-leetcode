ab1ddd09a3817d1ff1b87c637f57e1180d6b71d3
Tanisha Belkar <68585820+tanishabelkar@users.noreply.github.com>
2022-11-13 18:10:13 +0530

    Time: 711 ms (35.97%), Space: 144 MB (31.14%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,23 +1,24 @@
-class Solution 
+class Solution
 {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
-    {
-        priority_queue<pair<int, int>> pq;
-        vector<int> res;
-        for (int i = 0; i < k; ++i)
-            pq.push(make_pair(nums[i], i));
-        
-        res.push_back(pq.top().first);
-        
-        for (int i = k; i<nums.size(); ++i)
+    public:
+        vector<int> maxSlidingWindow(vector<int> &nums, int k)
         {
-            pq.push(make_pair(nums[i], i));
-            while (!pq.empty() && pq.top().second < i-k+1)
-                pq.pop();
-            res.push_back(pq.top().first);
+            priority_queue<pair<int, int>> pq;
+            vector<int> res(nums.size() - k + 1);
+            int j = 0;
+            for (int i = 0; i < k; ++i)
+                pq.push(make_pair(nums[i], i));
+
+            res[j++] = (pq.top().first);
+
+            for (int i = k; i < nums.size(); ++i)
+            {
+                pq.push(make_pair(nums[i], i));
+                while (pq.top().second < i - k + 1)
+                    pq.pop();
+                res[j++] = (pq.top().first);
+            }
+
+            return res;
         }
-        
-        return res;
-    }
 };
